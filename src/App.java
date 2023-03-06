bc2f49b137f0c4d03929563603176e8e47aaab0d
Andó Attila <andoattila698@gmail.com>
2023-03-06 16:50:46 +0100

    kezdés

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+
+
+
+    }
+}
